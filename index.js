8f40a7a77ecd37ac73b97cb018dfaded273c63ba
luzybry94 <luzybry94@gmail.com>
2021-02-03 16:57:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.getElementById('main').remove()
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Chaim is the champion'
